c01d0c14a0b2b17e1c7078ee6849d066beecbbc2
Toavina051 <hei.toavina.2@gmail.com>
2025-12-06 17:54:05 +0300

    feat:init my project and add the file init_db.sql

---
+++ TD1/init_db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE product_management_db;
+
+CREATE USER product_manager_user WITH PASSWORD '123456';
+
+GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
+
+GRANT USAGE ON SCHEMA public TO product_manager_user;
+
+GRANT CREATE ON SCHEMA public TO product_manager_user;
+
+GRANT SELECT, INSERT, DELETE, UPDATE ON ALL TABLES IN SCHEMA public TO product_manager;
+
+ALTER DEFAULTE PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO product_manager_user;
