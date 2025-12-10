78ce7f8801503a7710f979bd0beb1311a1a2818d
Toavina051 <hei.toavina.2@gmail.com>
2025-12-10 11:07:40 +0300

    fix:change user database product_manager to product_manager

--- TD1/init_db.sql
+++ TD1/init_db.sql
@@ -2,15 +2,15 @@ CREATE DATABASE product_management_db;
 
 CREATE USER product_manager_user WITH PASSWORD '123456';
 
-\c product_management_db
-
 GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
+
+\c product_management_db
 
 GRANT USAGE ON SCHEMA public TO product_manager_user;
 
 GRANT CREATE ON SCHEMA public TO product_manager_user;
 
-GRANT SELECT, INSERT, DELETE, UPDATE ON ALL TABLES IN SCHEMA public TO product_manager;
+GRANT SELECT, INSERT, DELETE, UPDATE ON ALL TABLES IN SCHEMA public TO product_manager_user;
 
-ALTER DEFAULTE PRIVILEGES IN SCHEMA public
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO product_manager_user;
